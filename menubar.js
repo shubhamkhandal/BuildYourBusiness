7897d74469ff5b0e0a17c40a186070daf1bb8662
Shubham khandal <shubhamshubhamkhandal@outlook.com>
2019-12-09 11:19:52 +0530

    files're added for public developers for develop for self

---
+++ menubar.js
@@ -0,0 +1,22 @@
+var mysidebar = document.getElementById("mySidebar");
+
+function opensidebar(){
+    if(mysidebar.style.display === 'block'){
+      mysidebar.style.display ='none';
+        
+    }
+    else{
+      mysidebar.style.display='block';
+        
+    }
+  }
+
+
+//close the sidebar.............................................................. 
+
+function closesidebar(){
+    mysidebar.style.display="none";
+}
+ 
+
+
